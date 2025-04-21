71055d057d2d68054bc6cff6ae3f920468ae767f
Galkina <darja.galkin@gmail.com>
2025-04-21 16:09:06 +0300

    Rewrite the title

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int a) {
-        int miles = a / 20;
-        return miles;
+        int result = a / 20;
+        return result;
     }
-
 }
